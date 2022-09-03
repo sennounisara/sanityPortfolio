94bed87f76eff7c8df7c6fe06b36c3c5d11a21e8
Sara Sennouni <sara.sennouni1214@gmail.com>
2022-09-03 07:30:38 -0700

    commit

---
+++ schemas/project.js
@@ -0,0 +1,35 @@
+export default {
+    name: 'project',
+    title: 'Project',
+    type: 'document',
+    fields: [
+      {
+        name: 'title',
+        title: 'Title',
+        type: 'string',
+      },
+      {
+        name: 'soustitle',
+        title: 'Sous-Title',
+        type: 'string',
+      },
+      {
+        name: 'description',
+        title: 'Description',
+        type: 'text',
+      },
+      {
+        name:'image',
+        title:"Image",
+        type:'array',
+        of:[
+            {
+                title: "Room Image",
+                name: "roomImage",
+                type: "image",
+            }
+        ]
+      },
+    ],
+  }
+  
