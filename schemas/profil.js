bf63b9bba612a2cf64958dad0e06339a130c725c
sara sennouni <sara.sennouni1996@gmail.com>
2022-12-01 18:11:14 +0100

    profil

---
+++ schemas/profil.js
@@ -0,0 +1,22 @@
+export default {
+    name:'profil',
+    title:'Profil',
+    type:'document',
+    fields:[
+        {
+            name: 'titleBlock',
+            title: 'Block Title',
+            type: 'string',
+        },
+        {
+            name: 'Description',
+            title: 'description',
+            type: 'text',
+        },
+        {
+            name: 'Image',
+            title: 'image',
+            type: 'image',
+        }
+    ]
+}
--- schemas/schema.js
+++ schemas/schema.js
@@ -12,6 +12,7 @@ import author from './author'
 import education from './education'
 import project from './project'
 import contact from './contact'
+import profil from './profil'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -26,6 +27,7 @@ export default createSchema({
     education,
     project,
     contact,
+    profil,
     // When added to this list, object types can be used as
     // { type: 'typename' } in other document schemas
     blockContent,
